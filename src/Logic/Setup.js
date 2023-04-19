244fb5e8a62d9bec02c4048de4403f21d31fcbad
J <59581793+daechi73@users.noreply.github.com>
2023-04-18 21:43:38 -0400

    changed the geocode function to pass in an cityname argument

--- src/Logic/Setup.js
+++ src/Logic/Setup.js
@@ -1,7 +1,7 @@
-const geocode = async () => {
+const geocode = async (city) => {
   const response = await fetch(
     `
-    http://api.openweathermap.org/geo/1.0/direct?q=london
+    http://api.openweathermap.org/geo/1.0/direct?q=${city}
     &limit=5&appid=8fc4fd101e5832e98fe2788ea8710650
     `,
     { mode: "cors" }
@@ -9,7 +9,7 @@ const geocode = async () => {
   const geocodeData = await response.json();
 
   const geocode = geocodeData.find((geocode) => {
-    return geocode.name.toLowerCase() === "london";
+    return geocode.name.toLowerCase() === city.toLowerCase();
   });
   return geocode;
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { geocode } from "./Logic/Setup";
 
-geocode().then((data) => {
+geocode("toronto").then((data) => {
   console.log(data);
 });
