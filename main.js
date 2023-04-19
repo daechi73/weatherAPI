2a4d9e005034ab468264b98a1fbb392c26745b95
J <59581793+daechi73@users.noreply.github.com>
2023-04-19 01:24:13 -0400

    basic css layout added; fetched information gets rendered into the correct elements

--- main.js
+++ main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/Logic/Setup.js":
+/*!****************************!*\
+  !*** ./src/Logic/Setup.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"geocode\": () => (/* binding */ geocode),\n/* harmony export */   \"oneCall\": () => (/* binding */ oneCall)\n/* harmony export */ });\nconst geocode = async (city) => {\n  const response = await fetch(\n    `\n    http://api.openweathermap.org/geo/1.0/direct?q=${city}\n    &limit=5&appid=8fc4fd101e5832e98fe2788ea8710650\n    `,\n    { mode: \"cors\" }\n  );\n  const geocodeData = await response.json();\n\n  const geocode = geocodeData.find((geocode) => {\n    return geocode.name.toLowerCase() === city.toLowerCase();\n  });\n  return geocode;\n};\n\nconst oneCall = async (geocode) => {\n  const { lat, lon } = geocode;\n\n  const response = await fetch(\n    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=8fc4fd101e5832e98fe2788ea8710650`,\n    { mode: \"cors\" }\n  );\n  const countryData = await response.json();\n\n  return countryData;\n};\n\n\n\n\n//# sourceURL=webpack://weatherapi/./src/Logic/Setup.js?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log(\"jaja\");\n\n\n//# sourceURL=webpack://vscoderepo/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _Logic_Setup__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Logic/Setup */ \"./src/Logic/Setup.js\");\n\n\n(0,_Logic_Setup__WEBPACK_IMPORTED_MODULE_0__.geocode)(\"toronto\").then((data) => {\n  (0,_Logic_Setup__WEBPACK_IMPORTED_MODULE_0__.oneCall)(data).then((country) => {\n    console.log(country.main.temp);\n  });\n});\n\n\n//# sourceURL=webpack://weatherapi/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
